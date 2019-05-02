6c86cc524178283c71bca00a7375206a84adf8fe
Ben <ben.speakman@peakon.com>
2019-05-02 11:24:40 +0100

    Add maps

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ if [ "$1" == "update" ]; then
     /home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/server +app_update 232330 +quit;
 fi
 
-/home/steam/server/srcds_run -game cstrike +maxplayers 16 +map gg_simpsons_h1 +hostname "$SERVER_HOSTNAME" +rcon_password "$RCON_PASSWORD";
+/home/steam/server/srcds_run -game cstrike +maxplayers 16 +map aim_spacewar +hostname "$SERVER_HOSTNAME" +rcon_password "$RCON_PASSWORD";
