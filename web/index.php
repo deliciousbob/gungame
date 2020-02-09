8c32b7f4fb8c50672eec5eae048c932e5831d631
Ben <ben.speakman@peakon.com>
2020-02-09 10:35:55 +0000

    Update index page

--- web/index.php
+++ web/index.php
@@ -18,18 +18,46 @@ $data = $Query->GetInfo();
   <title><?php echo $data['HostName']; ?></title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  <style>
+    body {
+      font-family: 'Open Sans', sans-serif;
+      color: #FFF;
+      background-color: #1E152A
+    }
+
+    h1 {
+      font-size: 16px;
+    }
+
+    #server {
+      position: fixed;
+      left: 50%;
+      top: 300px;
+      transform: translate(-50%, -50%);
+      margin: 0 auto;
+      font-size: 12px;
+      text-align: center;
+    }
+
+    img {
+      border: 1px solid #FFF;
+    }
+  </style>
 </head>
 
 <body>
 
-  <div>
-    <h1><?php echo $data['HostName']; ?></h1>
-    <img src="https://image.gametracker.com/images/maps/160x120/css/<?php echo $data['Map']; ?>.jpg" />
+  <div id="server">
+    <a href="https://www.gametracker.com/server_info/5.135.164.88:27015/">
+      <img src="https://image.gametracker.com/images/maps/160x120/css/<?php echo $data['Map']; ?>.jpg" />
+    </a>
+    <p><?php echo $data['HostName']; ?></p>
     <p>
-        <?php echo $data['Players']; ?> / <?php echo $data['MaxPlayers']; ?>
+      <?php echo $data['Players']; ?> / <?php echo $data['MaxPlayers']; ?>
     </p>
     <p>
-        <a href="steam://connect/5.135.164.88:27015">Join</a>
+      <a href="steam://connect/5.135.164.88:27015">Join</a>
     </p>
   </div>
 
