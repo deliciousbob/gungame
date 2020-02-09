7da6a956ef22287139c2640fb66a821aaf3e3dca
Ben <ben.speakman@peakon.com>
2020-02-09 10:24:05 +0000

    Add index page

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use xPaw\SourceQuery\SourceQuery;
+
+$Query = new SourceQuery();
+$Query->Connect('127.0.0.1', '27015');
+
+$data = $Query->GetInfo();
+
+?>
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $data['HostName']; ?></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+  <div>
+    <h1><?php echo $data['HostName']; ?></h1>
+    <img src="https://image.gametracker.com/images/maps/160x120/css/<?php echo $data['Map']; ?>.jpg" />
+    <p>
+        <?php echo $data['Players']; ?> / <?php echo $data['MaxPlayers']; ?>
+    </p>
+    <p>
+        <a href="steam://connect/5.135.164.88:27015">Join</a>
+    </p>
+  </div>
+
+</body>
+
+</html>
